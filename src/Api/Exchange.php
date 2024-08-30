f9710be7e4104c2544cd687dbe5e9d661c2f851f
minasm <minas@miroglu.com>
2024-08-30 18:05:31 +0100

    Add methods to retrieve exchange symbols and delisted tickers
    Introduced `list` and `delisted` methods to fetch supported exchange symbols and delisted tickers, respectively. Ensures correct URL segments and parameters are set for these operations. This enhancement offers improved management of exchange data.

--- src/Api/Exchange.php
+++ src/Api/Exchange.php
@@ -7,6 +7,7 @@ use RadicalLoop\Eod\Traits\Actionable;
 class Exchange extends EodClient
 {
     use Actionable;
+
     /**
      * url segment for exchange api
      *
@@ -17,41 +18,74 @@ class Exchange extends EodClient
     /**
      * Set Get Exchange Symbols api
      * url : https://eodhistoricaldata.com/knowledgebase/list-symbols-exchange/
-     * @param string $symbol
-     * @param array $params
+     *
+     * @param  string  $symbol
+     * @param  array  $params
      * @return Exchange
      */
     public function symbol($symbol, $params = [])
     {
         $this->setParams($symbol, $params);
+
         return $this;
     }
 
     /**
      * Set Multiple Tickers api
      * url: https://eodhistoricaldata.com/knowledgebase/multiple-tickers-download/
-     * @param string $symbol
-     * @param array $params
+     *
+     * @param  string  $symbol
+     * @param  array  $params
      * @return Exchange
      */
     public function multipleTicker($symbol, $params = [])
     {
         $this->urlSegment = '/eod-bulk-last-day';
         $this->setParams($symbol, $params);
+
         return $this;
     }
-    
-     /**
+
+    /**
      * Get exchange details api
      * url: https://eodhistoricaldata.com/financial-apis/exchanges-api-trading-hours-and-holidays/
-     * @param string $symbol
-     * @param array $params
+     *
+     * @param  string  $symbol
+     * @param  array  $params
      * @return Exchange
      */
     public function details($symbol, $params = [])
     {
         $this->urlSegment = '/exchange-details';
         $this->setParams($symbol, $params);
+
+        return $this;
+    }
+
+    /**
+     * Get the full list of supported exchanges symbols with names, codes, operating MICs, country, and currency
+     * url: https://eodhd.com/financial-apis/exchanges-api-list-of-tickers-and-trading-hours
+     */
+    public function list(string $exchange, array $params = []): self
+    {
+        $this->urlSegment = '/exchange-symbol-list';
+        $this->setParams($exchange, $params);
+
+        return $this;
+    }
+
+    /**
+     * Get the list of inactive (delisted) tickers from an exchange
+     * url: https://eodhd.com/financial-apis/delisted-stock-companies-data
+     */
+    public function delisted(string $exchange, array $params = []): self
+    {
+        $this->urlSegment = '/exchange-symbol-list';
+
+        $params = array_merge($params, ['delisted' => 1]);
+
+        $this->setParams($exchange, $params);
+
         return $this;
     }
 }
